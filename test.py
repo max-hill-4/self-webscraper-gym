23fe938e698bbdefd225409977013b33fd6aa86c
Max <maxwellhill2004@icloud.com>
2022-12-02 18:21:30 +0000

    added class system

--- test.py
+++ test.py
@@ -1,18 +1,21 @@
-
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-import time
+from details import EMAIL, PASSWORD
 driver = webdriver.Chrome("chromedriver")
 
 
-def login():
-    driver.get("https://members.energiefitness.com/Login")
-    driver.find_element(By.XPATH, '//*[@id="Email"]').send_keys(username)
+class GymApi:
+    def login():
+        driver.get("https://members.energiefitness.com/Login")
+        driver.find_element(By.XPATH, '//*[@id="Email"]').send_keys(EMAIL)
+        driver.find_element(By.XPATH, '//*[@id="Password"]').send_keys(PASSWORD)
+        driver.find_element(By.CSS_SELECTOR, 'button.mainbutton').click()
+        print("Logged in")
 
-    driver.find_element(By.XPATH, '//*[@id="Password"]').send_keys(password)
+    def get_people():
+        driver.get("https://members.energiefitness.com/lincoln-city/inthevenue")
+        poeple = driver.find_element(By.XPATH, '/html/body/main/div/div/h1').text
+        return poeple
 
-    #driver.find_elements(By.CLASS_NAME, 'mainbutton angular').click()
-    driver.find_element(By.CSS_SELECTOR, 'button.mainbutton').click()
-
-time.sleep(5)
-driver.quit()
+GymApi.login()
+print(GymApi.get_people())
